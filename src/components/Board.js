2974be461988ec05caecb6d6c238e768d7390b05
Hajar HKR <elhakourhajar@gmail.com>
2021-02-22 22:23:40 +0100

    Add page that always requires a logged-in user

--- src/components/Board.js
+++ src/components/Board.js
@@ -21,7 +21,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
-        MyTurn
+        Turn
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
